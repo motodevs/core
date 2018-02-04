544b96414b709f3d4b9cec1c92b9a870b306c40e
Yunus Oksuz <yunusoksuz@gmail.com>
2018-02-04 16:48:37 +0300

    celltower setters

--- src/main/java/com/openvehicletracking/core/CellTower.java
+++ src/main/java/com/openvehicletracking/core/CellTower.java
@@ -11,12 +11,30 @@ public class CellTower {
     private short lac;
     private int cellTower;
 
+    public CellTower() {
+    }
 
-    public CellTower(short mobileCountryCode, byte mobileNetworkCode, short locationAreaCode, int cellTowerId) {
-        mcc = mobileCountryCode;
-        mnc = mobileNetworkCode;
-        lac = locationAreaCode;
-        cellTower = cellTowerId;
+    public CellTower(short mcc, byte mnc, short lac, int cellTower) {
+        this.mcc = mcc;
+        this.mnc = mnc;
+        this.lac = lac;
+        this.cellTower = cellTower;
+    }
+
+    public void setMcc(short mcc) {
+        this.mcc = mcc;
+    }
+
+    public void setMnc(short mnc) {
+        this.mnc = mnc;
+    }
+
+    public void setLac(short lac) {
+        this.lac = lac;
+    }
+
+    public void setCellTower(int cellTower) {
+        this.cellTower = cellTower;
     }
 
     public short getMcc() {
