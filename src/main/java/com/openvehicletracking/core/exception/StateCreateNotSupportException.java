f7047c69278c90f9b11878c73b33440f399c8ef8
Yunus Oksuz <yunusoksuz@gmail.com>
2018-06-25 22:41:30 +0300

    StateCreateNotSupportException default constructors

--- src/main/java/com/openvehicletracking/core/exception/StateCreateNotSupportException.java
+++ src/main/java/com/openvehicletracking/core/exception/StateCreateNotSupportException.java
@@ -1,4 +1,15 @@
 package com.openvehicletracking.core.exception;
 
 public class StateCreateNotSupportException extends Exception {
+
+    public StateCreateNotSupportException() {
+    }
+
+    public StateCreateNotSupportException(String message) {
+        super(message);
+    }
+
+    public StateCreateNotSupportException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
